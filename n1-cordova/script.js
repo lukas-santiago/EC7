23a81c773ec9d4c57d5257517d3d165826fc6047
Lukas Santiago <lukas.asant@gmail.com>
2022-03-22 17:10:19 -0300

    changes #3

--- n1-cordova/script.js
+++ n1-cordova/script.js
@@ -1,23 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-	fetch('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0')
-		.then(res => res.json())
-		.then(data => {
-			let lista = data.results
-			let $pokemons = lista.map(async pokemon => {
-				let image = await fetch('https://pokeapi.co/api/v2/pokemon-form/' + pokemon.name + '/')
-					.then(res => res.json())
-					.then(data => data)
+  carregarPokemons();
+  $("body").on("click", "#lista-pokemons > div", (event) => {
+    location.href = "/n1-cordova/pages/details.html?pokemon=" + event.currentTarget.textContent.trim();
+  });
+});
 
+function carregarMais() {
+  carregarPokemons();
+}
+let offset = 0;
+async function carregarPokemons() {
+  let lista = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=" + offset)
+    .then((res) => res.json())
+    .then(async (data) => data.results);
 
-				$('#lista-pokemons').append(`
-                    <div>
-                        <img src="${image.sprites.front_default}" alt="">
-                        <p>${pokemon.name}</p>
-                    </div>
-                `)
-			})
-		})
-	$('body').on('click', '#lista-pokemons > div', event => {
-		location.href = '/n1-cordova/pages/details.html?pokemon=' + event.currentTarget.textContent.trim()
-	})
-})
+  let images = [];
+  let $pokemons = lista.map(async (pokemon) => {
+    let image = await fetch("https://pokeapi.co/api/v2/pokemon-form/" + pokemon.name + "/")
+      .then((res) => res.json())
+      .then((data) => data);
+
+    $("#lista-pokemons").append(`
+                <div>
+                    <img src="${image.sprites.front_default}" alt="">
+                    <p>${pokemon.name}</p>
+                </div>
+            `);
+  });
+  // await lista.forEach(async (pokemon) => {
+  //   let image = fetch("https://pokeapi.co/api/v2/pokemon-form/" + pokemon.name + "/")
+  //     .then((res) => res.json())
+  //     .then((data) => images.push(data));
+  // });
+  // console.log(images);
+
+  // //   let $pokemons = await ;
+
+  // $("#lista-pokemons").append(
+  //   images.map(
+  //     (image) => `
+  //   <div>
+  // 	  <img src="${image.sprites.front_default}" alt="">
+  // 	  <p>${pokemon.name}</p>
+  //   </div>
+  //   `
+  //   )
+  // );
+  offset += 10;
+}
