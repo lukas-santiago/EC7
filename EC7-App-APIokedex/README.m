350e0c60182bc5aefb8487c5a4403419871364f1
Lukas A. Santiago <lukas.asant@gmail.com>
2022-04-03 18:19:29 -0300

    Create README.m

---
+++ EC7-App-APIokedex/README.m
@@ -0,0 +1,7 @@
+###### Simple pokedex client with information about PokeAPI, Giphy, Pokemon TCG API and PoGo API. 
+Project to build a front-end application that can access public APIs with browser's Fetch API.
+APIs:
+- [PokéAPI](https://pokeapi.co/)
+- [Giphy](https://developers.giphy.com/docs/)
+- [PoGoAPI](https://pogoapi.net/documentation/)
+- [Pokemon TCG API](https://pokemontcg.io/)
